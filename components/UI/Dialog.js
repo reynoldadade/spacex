283ee9c8cba77b6e1f2e6741253ac1c95a18c781
reynoldadade <reynoldadade@gmail.com>
2021-12-02 09:17:40 +0000

    fix: overlay transition

--- components/UI/Dialog.js
+++ components/UI/Dialog.js
@@ -9,30 +9,39 @@ export default function MyDialog({ isOpen, setIsOpen }) {
         className="fixed z-10 inset-0 overflow-y-auto"
       >
         <div className="flex items-center justify-center min-h-screen">
-          <Dialog.Overlay className="fixed inset-0 bg-black opacity-70" />
+          <Transition.Child
+            as={Fragment}
+            enter=" duration-300 transition-opacity"
+            enterFrom="opacity-0"
+            leave="ease-in duration-300 transition-opacity"
+            leaveFrom="opacity-70"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-70" />
+          </Transition.Child>
 
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
+            enter="ease-out duration-500 transition transform"
+            enterFrom="opacity-0 scale-95 translate-x-5"
             enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
+            leave="ease-in duration-500 transition transform"
             leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
+            leaveTo="opacity-0 scale-95 translate-x-5"
           >
             <div className="relative bg-gray-900 rounded w-1/2 mx-auto p-4 shadow-lg">
               <Dialog.Title className="text-2xl py-2">
                 Payload Information
               </Dialog.Title>
 
-              <p>
+              <p className="p-1">
                 Are you sure you want to deactivate your account? All of your
                 data will be permanently removed. This action cannot be undone.
               </p>
 
               <button
                 onClick={() => setIsOpen(false)}
-                className="p-1 rounded bg-white text-black"
+                className="p-1 rounded bg-white text-black hover:bg-gray-700 hover:text-white"
               >
                 Close
               </button>
